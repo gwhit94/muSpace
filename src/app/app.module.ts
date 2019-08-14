9028bcba455cee4c4a97bf14804c1bc641cbcae2
Graham <greysnaa@gmail.com>
2019-08-14 10:12:27 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SearchComponent } from './search/search.component';
+import { MusicPlayerComponent } from './music-player/music-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    SearchComponent,
+    MusicPlayerComponent
   ],
   imports: [
     BrowserModule,
