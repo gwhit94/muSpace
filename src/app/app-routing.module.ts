079bf4f5ebed1d384e7ac7fc2d1b4495e182ecf0
Graham <greysnaa@gmail.com>
2019-08-16 09:56:56 -0500

    finished auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { MusicPlayerComponent } from './music-player/music-player.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import { SearchComponent } from './search/search.component';
+import { CanActivateRouteGuard } from './auth-guard.service';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignUpComponent},
-  { path: 'music-player', component: MusicPlayerComponent, },
-  { path: 'playlists', component: PlaylistComponent},
+  { path: 'music-player', component: MusicPlayerComponent, canActivate: [CanActivateRouteGuard]},
+  { path: 'playlists', component: PlaylistComponent, canActivate: [CanActivateRouteGuard]},
   { path: 'search', component: SearchComponent},
   { path: '**', component: LoginComponent}
 ];
