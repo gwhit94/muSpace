f8d2871445d4a490856f1213ccfd5980b9d4fd92
Graham <greysnaa@gmail.com>
2019-08-19 08:38:44 -0500

    Fixed auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import { SearchComponent } from './search/search.component';
+import { CanActivateRouteGuard } from './auth-guard.service';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignUpComponent},
   // { path: 'music-player', component: MusicPlayerComponent, },
-  { path: 'playlists', component: PlaylistComponent},
-  { path: 'search', component: SearchComponent},
+  { path: 'playlists', component: PlaylistComponent, canActivate: [CanActivateRouteGuard]},
+  { path: 'search', component: SearchComponent, canActivate: [CanActivateRouteGuard]},
   { path: '**', component: LoginComponent}
 ];
 
